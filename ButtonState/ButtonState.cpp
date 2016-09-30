2da179c0b8196ed05dd5215bb5519c0dc77a8477
grimmorium <marcin.pawlowski@aol.pl>
2016-09-30 19:25:16 +0200

    working version for simplearduinoexamples.com
    working without examples

--- ButtonState/ButtonState.cpp
+++ ButtonState/ButtonState.cpp
@@ -1,23 +1,27 @@
 /*
   ButtonState.h - Library for reading the state of buttons.
-  Created by Martin P. Pawlowski, January 28, 2015.
+  Created by Martin P. Pawlowski for simplearduinoexamples.com, September 30, 2016.
   Released into the public domain.
+  developed on Arduino 1.6.0
 */
 
 #include "Arduino.h"
 #include "ButtonState.h"
 
-ButtonState::ButtonState(short pin, unsigned long minMilSecShort, unsigned long maxMilSecShort, unsigned long minMilSecLong, unsigned long maxMilSecLong, unsigned long minMilSecBetween)
+ButtonState::ButtonState(short pin)
+{
+  _pin = pin;
+  _minMilSecShort = 20; 
+  _maxMilSecShort = 350;
+  _prevState = LOW;
+}
+
+ButtonState::ButtonState(short pin, unsigned long minMilSecShort, unsigned long maxMilSecShort)
 {
   _pin = pin;
   _minMilSecShort = minMilSecShort; 
   _maxMilSecShort = maxMilSecShort;
-  _minMilSecLong = minMilSecLong;
-  _wasPressed = false;
-  _milSecReleased = 0;
-  _minMilSecBetween = minMilSecBetween;
-  _buttonPushType = ButtonState::WAS_NOT_PRESSED;
-  _maxMilSecLong = maxMilSecLong;
+  _prevState = LOW;
 }
 
 void ButtonState::Init()
@@ -25,94 +29,60 @@ void ButtonState::Init()
   pinMode(_pin, INPUT);
 }
 
-void ButtonState::_getButtonPushType(unsigned long milSecPressed, unsigned long milSecReleased)
+short ButtonState::PushType()
 {
-	//TOO_SHORT_FOR_SHORT
-	if(milSecReleased - milSecPressed < _minMilSecShort)
-	{
-		_buttonPushType = ButtonState::TOO_SHORT_FOR_SHORT;
+	if(_milSecReleased - _milSecPressed > _maxMilSecShort){
+		return LONG_PRESSED;
 	}
-	//WAS_SHORT_PRESSED
-	//else 
-	if(milSecReleased - milSecPressed > _minMilSecShort && milSecReleased - milSecPressed < _maxMilSecShort)
-	{
-		_buttonPushType = ButtonState::WAS_SHORT_PRESSED;
+	if(_milSecReleased - _milSecPressed < _minMilSecShort){
+		return TRESHOLD;
 	}
-	//TOO_SHORT_FOR_LONG
-	//else 
-	if(milSecReleased - milSecPressed > _maxMilSecShort && milSecReleased - milSecPressed < _minMilSecLong)
-	{
-		_buttonPushType = ButtonState::TOO_SHORT_FOR_LONG;
+	if((_milSecReleased - _milSecPressed < _maxMilSecShort) && 
+	(_milSecReleased - _milSecPressed > _minMilSecShort)){
+		return SHORT_PRESSED;
 	}
-	//TOO_LONG_FOR_LONG
-	//else 
-	if(milSecReleased - milSecPressed > _minMilSecLong && milSecReleased - milSecPressed < _maxMilSecLong)
-	{
-		_buttonPushType = ButtonState::WAS_LONG_PRESSED;
-	}
-	//TOO_LONG_FOR_LONG
-	if (millis() - _milSecPressed > _maxMilSecLong )
-		  _buttonPushType = ButtonState::TOO_LONG_FOR_LONG;
+}
+
+boolean ButtonState::IsPressed()
+{
+	return _isPressed;
+}
+
+boolean ButtonState::WasReleased()
+{
+	return _wasReleased;
+}
+
+boolean ButtonState::WasPressed()
+{
+	return _wasPressed;
 }
 
 void ButtonState::CheckButton()
 {
-  //button was not pressed
-  if (_wasPressed == false &&  digitalRead(_pin) == LOW)
-  {
-	   _buttonPushType = ButtonState::WAS_NOT_PRESSED;
-  }
-  
-  //button still pressed
-  if (_wasPressed == true &&  digitalRead(_pin) == HIGH && (millis() - _milSecReleased > _minMilSecBetween))
-  {
-	  //check if it is a "long press"
-	  if (millis() - _milSecPressed > _maxMilSecLong )
-		  _buttonPushType = ButtonState::TOO_LONG_FOR_LONG;
-  }
-  
-  //button pressed event
-  if (_wasPressed == false &&  digitalRead(_pin) == HIGH && (millis() - _milSecReleased > _minMilSecBetween))
-  {
-	  _wasPressed = true;
-	  _milSecPressed = millis();
-  }
-  
-  //button released event
-  if (_wasPressed == true &&  digitalRead(_pin) == LOW)
-  {
-	  _wasPressed = false;
-	  _milSecReleased = millis();
-	  _getButtonPushType(_milSecPressed, _milSecReleased);
-  }
-  
-}
-
-short ButtonState::GetButtonState()
-{
-  switch (digitalRead(_pin)) {
-  case HIGH:
-    return ButtonState::IS_PRESSED;
-    break;
-  case LOW:
-    return ButtonState::IS_NOT_PRESSED;
-    break;
-  default: 
-    return ButtonState::IS_NOT_PRESSED;
-  }
-}
-
-short ButtonState::GetButtonPushType()
-{
-  return _buttonPushType;
+	_isPressed = false;
+	_wasReleased = false;
+	_wasPressed = false;
+	_curState = digitalRead(_pin);
+	
+	if(_curState==HIGH){
+		_isPressed = true;
+	}
+	
+	if(_prevState==HIGH && _curState==LOW){
+		_wasReleased = true;
+		_milSecReleased = millis();
+	}
+	
+	if(_prevState==LOW && _curState==HIGH){
+		_wasPressed = true;
+		_milSecPressed = millis();
+	}
+	
+	_prevState = _curState;
 }
 
 //static 
-const short ButtonState::IS_NOT_PRESSED = 1;
-const short ButtonState::IS_PRESSED = 2;
-const short ButtonState::WAS_NOT_PRESSED = 3;
-const short ButtonState::WAS_LONG_PRESSED = 4 ;
-const short ButtonState::WAS_SHORT_PRESSED = 5;
-const short ButtonState::TOO_SHORT_FOR_SHORT = 6;
-const short ButtonState::TOO_SHORT_FOR_LONG = 7;
-const short ButtonState::TOO_LONG_FOR_LONG = 8;
+const short ButtonState::LONG_PRESSED = 1;
+const short ButtonState::SHORT_PRESSED = 2;
+const short ButtonState::TRESHOLD = 3;
